b9ed42499fc3dbf2d3a21eb703f135f6311aa06e
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2020-12-03 22:57:13 +0300

    refactor

--- js/navBurger.js
+++ js/navBurger.js
@@ -1,17 +1,26 @@
-var menuBurger = document.querySelector(".a-burger");
-var menuAll = document.querySelector(".main-header_dropdown__menu");
-var closeMenu = document.querySelector(".main-header_dropdown__close");
+const menuBurger = document.querySelector(".a-burger");
+const menuAll = document.querySelector(".main-header_dropdown__menu");
+const closeMenu = document.querySelector(".main-header_dropdown__close");
+const section = document.getElementsByTagName('section');
 
 
 menuBurger.addEventListener("click", function (evt) {
     evt.preventDefault();
-    menuAll.classList.remove("closed_dropdown");
+    menuAll.classList.remove("close_dropdown");
     menuAll.classList.add("open_dropdown");
+    for (let i = 0; i < section.length; i++) {
+        section[i].style.filter = "blur(2px)";
+    }
+
+
 });
 
 
 closeMenu.addEventListener("click", function (evt) {
     evt.preventDefault();
     menuAll.classList.remove("open_dropdown");
-    menuAll.classList.add("closed_dropdown")
+    menuAll.classList.add("close_dropdown");
+    for (let i = 0; i < section.length; i++) {
+        section[i].style.filter = "none";
+    }
 });
